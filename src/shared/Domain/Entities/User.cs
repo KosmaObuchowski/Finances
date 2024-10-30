6cfa4a50f27755e9d4c26a878cf4b3f161b6539c
DESKTOP-TOIKBR9\Kosma <kosia3103@gmail.com>
2024-10-30 13:50:39 +0100

    a

---
+++ src/shared/Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public int Name { get; set; }
+        public int LastName { get; set; }
+        public int DOB { get; set; }
+        public int Status { get; set; }
+        public int temp1 { get; set; }
+        public int temp2 { get; set; }
+    }
+}
